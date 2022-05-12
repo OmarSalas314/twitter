3171686831831a68536419e477700323bcb2b17e
OmarSalas314 <omarsalasvieyra@gmail.com>
2022-05-12 14:22:11 -0600

    Paso 7 agregando requerimiento 2 (mod y prueba)

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,10 +2,9 @@ const User = require('./../../app/models/User')
 
 describe("Unit Test for User class", () => {
 
-    test('Create an User object', () => {
+    test('Requerimiento 1 Create an User object', () => {
         //Aqui invocamos el codigo que vas a usar en tu app
-        const user = new User(1, "carlogilmal", "Carlo", "Bio", "dateCreated", 
-        "lastUpdate")
+        const user = new User(1, "carlogilmal", "Carlo", "Bio")
 
         //Aqui validas los resultados de ese codigo
         //Esta es una compracion que va a igualar a ese valor de la izquierda con
@@ -14,7 +13,13 @@ describe("Unit Test for User class", () => {
         expect(user.username).toBe("carlogilmal")
         expect(user.name).toBe("Carlo")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdate")
-    });
+        //expect(user.dateCreated).toBe("dateCreated")
+        //expect(user.lastUpdated).toBe("lastUpdate")
+    })
+
+    test('Requerimiento 2: Fechas en atributos de User', () => {
+        const user = new User(1, "carlogilmal", "Carlo", "Bio",)
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
+    })
 })
